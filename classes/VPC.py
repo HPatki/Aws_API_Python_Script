90d7365c0c759694efaec0e614573bff33b6fba6
sanika <harsh.patki@gmail.com>
2018-12-07 16:30:45 +1100

    added new classes for RouteTable

--- classes/VPC.py
+++ classes/VPC.py
@@ -1,4 +1,5 @@
 import boto3
+import RouteTable
 
 class VPC:
 
@@ -8,8 +9,11 @@ class VPC:
     def tag (self, tags):
         self.__vpc.create_tags(Tags=[tags])
 
-    def attachIGW (self,igw,runType=False):
-        self.__vpc.attach_internet_gateway (DryRun=runType,InternetGatewayId=igw.getId())
+    def attachIGW (self,igw,dryRun=False):
+        self.__vpc.attach_internet_gateway (DryRun=dryRun,InternetGatewayId=igw.getId())
 
     def detachIGW (self,igw):
         self.__vpc.detach_internet_gateway (InternetGatewayId=igw.getId())
+		
+	def createRouteTable(self,dryRun=False):
+		return RouteTable.RouteTable(self.__vpc.create_route_table (dryRun))
