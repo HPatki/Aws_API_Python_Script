727be10e4671af521f4f7b876979803558a62d8f
sanika <harsh.patki@gmail.com>
2018-12-09 09:32:40 +1100

    individual methods for different options in creating route_table

--- classes/RouteTable.py
+++ classes/RouteTable.py
@@ -8,19 +8,79 @@ class RouteTable:
     def tag(self,tag):
         self.__rt.create_tags(Tags=[tag])
 
-    def createRoute(self,destCidrBlock, destIpv6CidrBlock, dryRun, target):
-        DestinationIpv6CidrBlock = destIpv6CidrBlock
-        DryRun = dryRun
+    def createRouteTarget_Egress(self,destCidrBlock, target, destIpv6CidrBlock=None, dryRun=False):
+        route = None
+        if destIpv6CidrBlock != None:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                   DestinationIpv6CidrBlock=destIpv6CidrBlock,
+                                   DryRun=dryRun,EgressOnlyInternetGatewayId=target)
+        else:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                   DryRun=dryRun, EgressOnlyInternetGatewayId=target)
 
-        #any be only one of these
-        if target.get('EgressOnlyInternetGatewayId'):
-            self.__rt.create_route( DestinationCidrBlock = destCidrBlock,
-                                    .get('DestinationCidrBlock') if target.get('DestinationCidrBlock')
-        GatewayId = args.get('DestinationCidrBlock']
-        NatInstanceId = args.get('DestinationCidrBlock']
-        NatGatewayId = args.get('DestinationCidrBlock']
-        TransitGatewayId = args.get('DestinationCidrBlock']
-        NetworkInterfaceId = args.get('DestinationCidrBlock']
-        VpcPeeringConnectionId = args.get('DestinationCidrBlock']
+        return route
 
-        self.__rt.create_route()
+    def createRouteTarget_Gateway(self, destCidrBlock, target, destIpv6CidrBlock=None, dryRun=False):
+        route = None
+        if destIpv6CidrBlock != None:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                            DestinationIpv6CidrBlock=destIpv6CidrBlock,
+                                            DryRun=dryRun, GatewayId=target)
+        else:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                               DryRun=dryRun, GatewayId=target)
+
+        return route
+
+    def createRouteTarget_NatInstance(self, destCidrBlock, target, destIpv6CidrBlock=None, dryRun=False):
+        route = None
+        if destIpv6CidrBlock != None:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DestinationIpv6CidrBlock=destIpv6CidrBlock,
+                                           DryRun=dryRun, InstanceId=target)
+        else:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DryRun=dryRun, InstanceId=target)
+
+
+        return route
+
+    def createRouteTarget_NatGateway(self, destCidrBlock, target, destIpv6CidrBlock=None, dryRun=False):
+        route = None
+        if destIpv6CidrBlock != None:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DestinationIpv6CidrBlock=destIpv6CidrBlock,
+                                           DryRun=dryRun, NatGatewayId=target)
+        else:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DryRun=dryRun, NatGatewayId=target)
+
+    def createRouteTarget_TransitGateway(self, destCidrBlock, target, destIpv6CidrBlock=None, dryRun=False):
+        route = None
+        if destIpv6CidrBlock != None:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DestinationIpv6CidrBlock=destIpv6CidrBlock,
+                                           DryRun=dryRun, TransitGatewayId=target)
+        else:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DryRun=dryRun, TransitGatewayId=target)
+
+    def createRouteTarget_NetworkInterface(self, destCidrBlock, target, destIpv6CidrBlock=None, dryRun=False):
+        route = None
+        if destIpv6CidrBlock != None:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DestinationIpv6CidrBlock=destIpv6CidrBlock,
+                                           DryRun=dryRun, NetworkInterfaceId=target)
+        else:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DryRun=dryRun, NetworkInterfaceId=target)
+
+    def createRouteTarget_VpcPeer(self, destCidrBlock, target, destIpv6CidrBlock=None, dryRun=False):
+        route = None
+        if destIpv6CidrBlock != None:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DestinationIpv6CidrBlock=destIpv6CidrBlock,
+                                           DryRun=dryRun, VpcPeeringConnectionId=target)
+        else:
+            route = self.__rt.create_route(DestinationCidrBlock=destCidrBlock,
+                                           DryRun=dryRun, VpcPeeringConnectionId=target)
